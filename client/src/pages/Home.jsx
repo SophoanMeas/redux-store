da9b7c80ce9eea5de6c0fef0d87b9b8a21090d87
Sophoan Meas <sophoan.meas@hotmail.com>
2022-07-23 01:32:32 -0400

    add home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductList from "../components/ProductList";
+import CategoryMenu from "../components/CategoryMenu";
+import Cart from "../components/Cart";
+
+export default function Home() {
+    return(
+        <div className ='container'>
+        <CategoryMenu/>
+        <ProductList/>
+        <Cart/>
+        </div>
+    )
+}
